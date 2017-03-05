afc3d7cf5e3b936ecac973fed66cecdd618a78c6
Rodrigo Guinea <jrguinea@gmail.com>
2017-03-05 12:20:08 -0600

    R-Programming assignment, Matrix Inverse with cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming exercise for R-Programming week#3
+## Based on the functions example, the solution has to determine 
+## if a matrix inverse has been done, if it has been done,
+## then the calculation doesn't has to do it, and pull from cache
+## otherwise, it has to be calculated
 
-## Write a short comment describing this function
+## basic functions for the matrix that we'll work on
+## set the values of the matrix, get the values,
+## calculate the "Solve" (matrix invertion)
+## get the results of the invertion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ix <- NULL
+        set <- function(y) {
+                x <<- y
+                ix <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) ix <<- solve
+        getinv <- function() ix
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ix <- x$getinv()
+        if(!is.null(ix)) {
+                message("getting cached data")
+                return(ix)
+        }
+        data <- x$get()
+        ix <- solve(data, ...)
+        x$setinv(ix)
+        ix
 }
